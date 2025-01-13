f9e4a09c8904a576d4341d81e9efc9166c77f27f
shaxa98 <shakhzodakhrorov@gmail.com>
2025-01-13 12:43:08 +0500

    update Navbar.jsx

--- Food/src/components/navbar/Navbar.jsx
+++ Food/src/components/navbar/Navbar.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 import "./Navbar.css";
+import { assets } from "../../assets/assets";
 function Navbar() {
-  return <div className="navbar"></div>;
+  return (
+    <div className="navbar">
+      <img src={assets.logo} alt="" className="logo" />
+      <ul className="navbar-menu">
+        <li>home</li>
+        <li>menu</li>
+        <li>mobile-app</li>
+        <li>contact us</li>
+      </ul>
+      <div className="navbar-right">
+        <img src={assets.search_icon} alt="" />
+        <div className="navbar-search-icon">
+            <img src={} alt="" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
