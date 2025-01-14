28577c1a4863dbf35031bf147f8851fd9672e701
shaxa98 <shakhzodakhrorov@gmail.com>
2025-01-14 10:22:50 +0500

    update navbar

--- Food/src/components/navbar/Navbar.jsx
+++ Food/src/components/navbar/Navbar.jsx
@@ -1,15 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 import { assets } from "../../assets/assets";
 function Navbar() {
+  const [menu, setMenu] = useState("menu");
   return (
     <div className="navbar">
       <img src={assets.logo} alt="" className="logo" />
       <ul className="navbar-menu">
-        <li>home</li>
-        <li>menu</li>
-        <li>mobile-app</li>
-        <li>contact us</li>
+        <li className={menu === "home" ? "active" : ""}>home</li>
+        <li className={menu === "menu" ? "active" : ""}>menu</li>
+        <li className={menu === "mobile-app" ? "active" : ""}>mobile-app</li>
+        <li className={menu === "contact us" ? "active" : ""}>contact us</li>
       </ul>
       <div className="navbar-right">
         <img src={assets.search_icon} alt="" />
