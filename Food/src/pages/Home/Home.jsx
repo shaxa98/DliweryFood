469be01e1ed92a2977be5b101242d67c47648498
shaxa98 <shakhzodakhrorov@gmail.com>
2025-01-15 15:43:12 +0500

    add explore menu

--- Food/src/pages/Home/Home.jsx
+++ Food/src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "../../components/Header/Header";
 import "./Home.css";
+import ExploreMenu from "../../components/ExploreMenu/ExploreMenu";
 
 function Home() {
   return (
     <div>
       <Header />
+      <ExploreMenu />
     </div>
   );
 }
