0aac75bf6dcdefab21490eabf794d68e8137ae55
shaxa98 <shakhzodakhrorov@gmail.com>
2025-01-23 18:44:07 +0500

    add FoodDisplay folder

---
+++ Food/src/components/FoodDisplay/FoodDisplay.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./FoodDisplay.css";
+
+function FoodDisplay() {
+  return <div></div>;
+}
+
+export default FoodDisplay;
--- Food/src/context/StoreContext.jsx
+++ Food/src/context/StoreContext.jsx
@@ -4,7 +4,9 @@ import { food_list } from "../assets/assets";
 export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
-  const contextValue = {};
+  const contextValue = {
+    food_list,
+  };
   return (
     <StoreContext.Provider value={contextValue}>
       {props.children}
