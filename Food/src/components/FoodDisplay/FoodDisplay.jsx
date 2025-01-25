f3fdc3607249040fdbec1bdbd23fb012af5667ef
shaxa98 <shakhzodakhrorov@gmail.com>
2025-01-25 21:22:26 +0500

    update foodDisplay.jsx

--- Food/src/components/FoodDisplay/FoodDisplay.jsx
+++ Food/src/components/FoodDisplay/FoodDisplay.jsx
@@ -7,6 +7,11 @@ function FoodDisplay({ category }) {
   return (
     <div className="food-display" id="food-display">
       <h2>Top dishes near you</h2>
+      <div className="food-display-list">
+        {food_list.map((item, index) => {
+          return;
+        })}
+      </div>
     </div>
   );
 }
