88c8666ecbb2fb443f958b42c6469de33fb941e5
shaxa98 <shakhzodakhrorov@gmail.com>
2025-01-25 21:05:18 +0500

    update FoodDisplay.jsx

--- Food/src/components/FoodDisplay/FoodDisplay.jsx
+++ Food/src/components/FoodDisplay/FoodDisplay.jsx
@@ -4,7 +4,11 @@ import { StoreContext } from "../../context/StoreContext";
 
 function FoodDisplay() {
   const { food_list } = useContext(StoreContext);
-  return <div></div>;
+  return (
+    <div className="food-display" id="food-display">
+      <h2>Top dishes near you</h2>
+    </div>
+  );
 }
 
 export default FoodDisplay;
