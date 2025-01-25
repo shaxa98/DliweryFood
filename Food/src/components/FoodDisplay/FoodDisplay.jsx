a5f42c212f1b127e1479ae3c6423fc85ae013171
shaxa98 <shakhzodakhrorov@gmail.com>
2025-01-25 21:10:28 +0500

    ...

--- Food/src/components/FoodDisplay/FoodDisplay.jsx
+++ Food/src/components/FoodDisplay/FoodDisplay.jsx
@@ -2,7 +2,7 @@ import React, { useContext } from "react";
 import "./FoodDisplay.css";
 import { StoreContext } from "../../context/StoreContext";
 
-function FoodDisplay() {
+function FoodDisplay({ category }) {
   const { food_list } = useContext(StoreContext);
   return (
     <div className="food-display" id="food-display">
