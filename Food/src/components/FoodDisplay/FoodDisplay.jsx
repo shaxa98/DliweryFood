0848c916210c2029699ec8608b467600c604e707
shaxa98 <shakhzodakhrorov@gmail.com>
2025-01-27 20:47:29 +0500

    update FoodDisplay.jsx

--- Food/src/components/FoodDisplay/FoodDisplay.jsx
+++ Food/src/components/FoodDisplay/FoodDisplay.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import "./FoodDisplay.css";
 import { StoreContext } from "../../context/StoreContext";
+import FoodItem from "../FoodItem/FoodItem";
 
 function FoodDisplay({ category }) {
   const { food_list } = useContext(StoreContext);
@@ -9,7 +10,16 @@ function FoodDisplay({ category }) {
       <h2>Top dishes near you</h2>
       <div className="food-display-list">
         {food_list.map((item, index) => {
-          return;
+          return (
+            <FoodItem
+              key={index}
+              id={item._id}
+              name={item.name}
+              description={item.description}
+              price={item.price}
+              image={item.image}
+            />
+          );
         })}
       </div>
     </div>
