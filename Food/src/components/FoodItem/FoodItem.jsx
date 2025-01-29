80e6d8e018128bf49874997d57221c7a10022cd3
shaxa98 <shakhzodakhrorov@gmail.com>
2025-01-29 17:27:57 +0500

    update

--- Food/src/components/FoodItem/FoodItem.jsx
+++ Food/src/components/FoodItem/FoodItem.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import "./FoodItem.css";
 import { assets } from "../../assets/assets";
 function FoodItem({ id, name, price, description, image }) {
+  const [itemCount, setItemCount] = useState(0);
+
   return (
     <div className="food-item">
       <div className="food-item-img-container">
@@ -11,6 +13,28 @@ function FoodItem({ id, name, price, description, image }) {
         <div className="food-item-name-rating">
           <p>{name} </p>
           <img src={assets.rating_starts} alt="" />
+          {!itemCount ? (
+            <img
+              className="add"
+              src={assets.add_icon_white}
+              onClick={() => setItemCount((prev) => prev + 1)}
+              alt=""
+            />
+          ) : (
+            <div className="food-item-counter">
+              <img
+                onClick={() => setItemCount((prev) => prev - 1)}
+                src={assets.remove_icon_red}
+                alt=""
+              />
+              <p>{itemCount} </p>
+              <img
+                onClick={() => setItemCount((prev) => prev + 1)}
+                src={assets.add_icon_green}
+                alt=""
+              />
+            </div>
+          )}
         </div>
         <p className="food-item-desc">{description}</p>
         <p className="food-item-price">${price} </p>
