85efb59096d35a0ec4601fdc863ff7e8a3ae84c5
shaxa98 <shakhzodakhrorov@gmail.com>
2025-01-29 18:04:00 +0500

    fixed fooditem.jsx

--- Food/src/components/FoodItem/FoodItem.jsx
+++ Food/src/components/FoodItem/FoodItem.jsx
@@ -8,33 +8,33 @@ function FoodItem({ id, name, price, description, image }) {
     <div className="food-item">
       <div className="food-item-img-container">
         <img className="food-item-image" src={image} alt="" />
+        {!itemCount ? (
+          <img
+            className="add"
+            src={assets.add_icon_white}
+            onClick={() => setItemCount((prev) => prev + 1)}
+            alt=""
+          />
+        ) : (
+          <div className="food-item-counter">
+            <img
+              onClick={() => setItemCount((prev) => prev - 1)}
+              src={assets.remove_icon_red}
+              alt=""
+            />
+            <p>{itemCount} </p>
+            <img
+              onClick={() => setItemCount((prev) => prev + 1)}
+              src={assets.add_icon_green}
+              alt=""
+            />
+          </div>
+        )}
       </div>
       <div className="food-item-info">
         <div className="food-item-name-rating">
           <p>{name} </p>
           <img src={assets.rating_starts} alt="" />
-          {!itemCount ? (
-            <img
-              className="add"
-              src={assets.add_icon_white}
-              onClick={() => setItemCount((prev) => prev + 1)}
-              alt=""
-            />
-          ) : (
-            <div className="food-item-counter">
-              <img
-                onClick={() => setItemCount((prev) => prev - 1)}
-                src={assets.remove_icon_red}
-                alt=""
-              />
-              <p>{itemCount} </p>
-              <img
-                onClick={() => setItemCount((prev) => prev + 1)}
-                src={assets.add_icon_green}
-                alt=""
-              />
-            </div>
-          )}
         </div>
         <p className="food-item-desc">{description}</p>
         <p className="food-item-price">${price} </p>
