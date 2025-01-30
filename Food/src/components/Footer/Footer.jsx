c9d37820d343835cb88f4fec616049e6fbb79d56
shaxa98 <shakhzodakhrorov@gmail.com>
2025-01-30 12:22:24 +0500

    update footer.jsx

--- Food/src/components/Footer/Footer.jsx
+++ Food/src/components/Footer/Footer.jsx
@@ -1,8 +1,35 @@
 import React from "react";
 import "./Footer.css";
+import { assets } from "../../assets/assets";
 
 function Footer() {
-  return <div className="footer"></div>;
+  return (
+    <div className="footer" id="footer">
+      <div className="footer-content">
+        <div className="footer-content-left">
+          <img src={assets.logo} alt="" />
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum.
+          </p>
+          <div className="footer-social-icons">
+            <img src={assets.facebook_icon} alt="" />
+            <img src={assets.twitter_icon} alt="" />
+            <img src={assets.linkedin_icon} alt="" />
+          </div>
+        </div>
+        <div className="footer-content-center"></div>
+        <div className="footer-content-right"></div>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
