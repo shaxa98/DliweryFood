9102ac6ed4947e8e64ffa1155d466651d2dd27dd
shaxa98 <shakhzodakhrorov@gmail.com>
2025-02-13 20:37:57 +0500

    create mongoDB database

--- Backend/server.js
+++ Backend/server.js
@@ -15,3 +15,4 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Server started on http://localhost:${port}`);
 });
+//mongodb+srv://shakhzodakhrorov:DGiQRMJrXc4DCtrd@cluster0.aewzd.mongodb.net/?
