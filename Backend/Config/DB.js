e64ecbfefb337179d4065eb0c1be6dd1d80698c7
shaxa98 <shakhzodakhrorov@gmail.com>
2025-02-13 20:53:06 +0500

    update backend

--- Backend/Config/DB.js
+++ Backend/Config/DB.js
@@ -1,1 +1,9 @@
 import mongoose from "mongoose";
+
+export const connectDB = async () => {
+  await mongoose
+    .connect(
+      "mongodb+srv://shakhzodakhrorov:DGiQRMJrXc4DCtrd@cluster0.aewzd.mongodb.net/food-del"
+    )
+    .then(() => console.log("DB Connected"));
+};
