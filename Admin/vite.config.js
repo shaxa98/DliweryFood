66d158815608a183f8131ba46f77e034243befd1
shaxa98 <shakhzodakhrorov@gmail.com>
2025-02-20 18:13:59 +0500

    update admin panel

--- Admin/vite.config.js
+++ Admin/vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // This allows access from other devices
+    port: 5174, // This ensures the server runs on port 5173
+  }
 })
